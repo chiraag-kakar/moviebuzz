901c8d09133507ce17019732f953a4a7ccefcac2
chiraag-kakar <chirucos1998@gmail.com>
2020-10-13 00:26:17 +0530

    Updates

---
+++ precache-manifest.4f03bdc51d67a7d169b6bb5313bf334e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf9c84f3310dcfc1645f06eaf909a8f",
+    "url": "/moviebuzz/index.html"
+  },
+  {
+    "revision": "77fe6de96cc9e6541bf4",
+    "url": "/moviebuzz/static/css/main.1b2368c0.chunk.css"
+  },
+  {
+    "revision": "70fa7c8f5f2b19ad7bdb",
+    "url": "/moviebuzz/static/js/2.43093e81.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/moviebuzz/static/js/2.43093e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fe6de96cc9e6541bf4",
+    "url": "/moviebuzz/static/js/main.cdd39752.chunk.js"
+  },
+  {
+    "revision": "e52591d014ae73d7e914",
+    "url": "/moviebuzz/static/js/runtime-main.b7f2c592.js"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/moviebuzz/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/moviebuzz/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/moviebuzz/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/moviebuzz/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/moviebuzz/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/moviebuzz/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/moviebuzz/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/moviebuzz/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/moviebuzz/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/moviebuzz/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/moviebuzz/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/moviebuzz/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/moviebuzz/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/moviebuzz/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/moviebuzz/static/media/fa-solid-900.bea989e8.woff"
+  }
+]);
